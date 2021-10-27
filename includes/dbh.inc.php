5c039c545c41f5d32d4a3a91d5afd74e253606b1
Povilas Bikmanas <p.bikmanas@gmail.com>
2021-10-27 15:12:43 -0500

    Heroku deploy

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,14 @@
 <?php
-$dBServername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "loginsystem";
 
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
 
-$conn = mysqli_connect($dBServername, $dBUsername, $dBPassword, $dBName);
-
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
